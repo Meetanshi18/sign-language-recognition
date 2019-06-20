a31825318c6e9698eae2ef3770ddd4f6cee4926b
Meetanshi Mittal <meetanshi.18@gmail.com>
2019-06-21 01:51:50 +0530

    more functions

--- numpyTrials.py
+++ numpyTrials.py
@@ -20,3 +20,18 @@ print(a.itemsize)
 b = np.array([(1,2,3) , (4,5,6)])
 print(b)
 print( b > 2 )
+print( b * 3 )
+
+zeroMatrix = np.zeros((3, 2))
+print(zeroMatrix)
+print(np.ones((10)))
+
+a = np.array([2,3,4], dtype = np.int16)
+print(a.dtype)
+print(np.random.random((2,3)))
+
+a = np.random.randint(0,10,6)
+a = a.reshape(3,2)
+print(a)
+print(a.sum( axis = 1 )) # sum(), mean(), var(), std() all accept axis parameter
+print(a.sum( axis = 0 ))
