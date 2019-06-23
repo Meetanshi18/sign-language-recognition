4272dc8871db69b3d8d67821dfb96ba695b7a42a
Meetanshi Mittal <meetanshi.18@gmail.com>
2019-06-24 02:27:17 +0530

    implemented svm

---
+++ svm.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+# print(digits.data)
+# print(digits.target)
+# print(digits.data[-6])
+clf = svm.SVC(gamma = 0.0001, C=100)
+
+print(len(digits.data))
+
+x,y = digits.data[:-10], digits.target[:-10]
+
+clf.fit(x,y)
+
+print('Prediction: ', clf.predict(digits.data[-1:]))
+plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
