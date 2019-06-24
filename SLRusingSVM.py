0fa210a285a2ad44a3162674a2aa041108468aa8
Meetanshi Mittal <meetanshi.18@gmail.com>
2019-06-24 17:25:55 +0530

    running svm implementation

--- SLRusingSVM.py
+++ SLRusingSVM.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import cv2
 
 train = pd.read_csv('C:/Users/meeta/Desktop/TSWE2019/sign-language-recognition/dataset/sign_mnist_train.csv', header = 0)
 test = pd.read_csv('dataset/sign_mnist_test.csv', header = 0)
@@ -25,5 +26,6 @@ clf.fit(x_train, y_train)
 print('Prediction: ', clf.predict(x_test[20:21]))
 
 x_test = np.array([np.reshape(i, (28, 28)) for i in x_test])
-plt.imshow(np.array(x_test[20]), interpolation='nearest')
+img = cv2.cvtColor(x_test[20], cv2.COLOR_BGR2RGB)
+plt.imshow(np.array(img), interpolation='nearest')
 plt.show()
