2b82e34e9117c6633526164720c567c5c9c47741
Meetanshi Mittal <meetanshi.18@gmail.com>
2019-06-24 11:28:24 +0530

    update svm.py

--- svm.py
+++ svm.py
@@ -12,9 +12,11 @@ clf = svm.SVC(gamma = 0.0001, C=100)
 print(len(digits.data))
 
 x,y = digits.data[:-10], digits.target[:-10]
+# print(x[:5])
+print(y[:5])
 
-clf.fit(x,y)
+# clf.fit(x,y)
 
 print('Prediction: ', clf.predict(digits.data[-1:]))
-plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
-plt.show()
+# plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
+# plt.show()
